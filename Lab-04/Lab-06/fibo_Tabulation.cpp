4f3615aa3f5eb29d13559b36a43c92aaae466f79
Turja Chakraborty <152412436+Turjacb1@users.noreply.github.com>
2025-06-27 01:02:14 +0600

    prims

---
+++ Lab-04/Lab-06/fibo_Tabulation.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+int fiboonacchi(int n){
+    
+    if (n <= 1)
+        return n;
+
+   
+    vector<int> dp(n + 1);
+
+    
+    dp[0] = 0;
+    dp[1] = 1;
+
+    
+    for (int i = 2; i <= n; ++i){
+
+       
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+   
+    return dp[n];
+}
+
+int main(){
+    int n ;
+    cin>>n;
+    int result = fiboonacchi(n);
+
+    cout << result << endl;
+
+    return 0;
+}
