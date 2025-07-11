5e5344bc580b00b284180304a64b53810873b643
Turja Chakraborty <152412436+Turjacb1@users.noreply.github.com>
2025-07-11 15:27:21 +0600

    some changes

--- Lab-10-BrackTracking/Queen.cpp
+++ Lab-10-BrackTracking/Queen.cpp
@@ -2,11 +2,9 @@
 #include <vector>
 using namespace std;
 
-#define N 4
-
-void print(vector<vector<int>>& board) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+void print(vector<vector<int>>& board, intN) {
+    for (int i =0;i <N;i++) {
+        for (int j =0; j <N; j++) {
             cout << (board[i][j] ? "Q " : ". ");
         }
         cout << endl;
@@ -14,49 +12,56 @@ void print(vector<vector<int>>& board) {
     cout << endl;
 }
 
-bool safe(vector<vector<int>>& board, int row, int col) {
+bool safe(vector<vector<int>>& board,int row,int col,int N) {
     
-    for (int i = 0; i < row; i++)
+    for (int i=0; i <row;i++)
         if (board[i][col])
             return false;
 
     
-    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+    for (int i=row,j=col; i >=0 && j >=0; i--,j--)
         if (board[i][j])
             return false;
 
     
-    for (int i = row, j = col; i >= 0 && j < N; i--, j++)
+    for (int i=row,j =col;i >=0 && j <N;i--,j++)
         if (board[i][j])
             return false;
 
     return true;
 }
 
-bool queen(vector<vector<int>>& board, int row) {
+bool queen(vector<vector<int>>& board,int row,int N) {
     if (row == N) {
-        print(board); 
-        return true;   
+        print(board, N);
+        return true;
     }
 
-    for (int col = 0; col < N; col++) {
-        if (safe(board, row, col)) {
-            board[row][col] = 1;                    
-            if (queen(board, row + 1))  
+    for (int col =0;col<N;col++) {
+        if (safe(board, row, col, N)) {
+            board[row][col] = 1;
+            if (queen(board, row + 1, N))
                 return true;
-            board[row][col] = 0; 
+            board[row][col] = 0;
         }
     }
 
-    return false;  
+    return false;
 }
 
 int main() {
-    vector<vector<int>> board(N, vector<int>(N, 0));  
+    int N;
+    cout << " board size  ";
+    cin >> N;
+    if (N <= 0) {
+        cout << "invalid input.\n";
+        return 1;
+    }
+    vector<vector<int>> board(N, vector<int>(N, 0));
 
-    cout << "5-Queens Problem:\n\n";
-    if (!queen(board, 0)) {
-        cout << "No solution exists.\n";
+    cout <<N<<"-queens problem:\n\n";
+    if (!queen(board, 0, N)) {
+        cout << "no solution.\n";
     }
 
     return 0;
